003785bcae2b039e508259e42a68c4cf61694430
Jaison <jaisonpjohn@gmail.com>
2025-05-11 00:39:52 -0700

    a simple tailwind page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+<style lang="postcss">
+    @reference "tailwindcss";
+    :global(html) {
+        background-color: theme(--color-green-100);
+    }
+</style>
